b6d6ddb9acad56039d78ea2034b3df73adf6b89a
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-02-21 11:48:41 +0530

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { resolve } = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = 3010;
+
+const config ={
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvium: process.env.IS_KALVIUM === 'true' ,
+
+};
+
+app.use(express.static('static'));
+console.log(config);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports =config;
