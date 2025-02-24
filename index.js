e18a64e3136b79ba4327a3be2fe0a7be4477b74e
sagar2709 <179591921+KALVIUM23@users.noreply.github.com>
2025-02-24 11:15:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const { resolve } = require('path');
-<<<<<<< HEAD
+
 require('dotenv').config();
-=======
->>>>>>> 2c112e290717192f46429debd90bb4a738969949
+
 
 const app = express();
 const port = 3010;
-
-<<<<<<< HEAD
 const config ={
   apiKey: process.env.API_KEY,
   serverSecret: process.env.SERVER_SECRET,
@@ -19,9 +16,9 @@ const config ={
 app.use(express.static('static'));
 console.log(config);
 
-=======
+
 app.use(express.static('static'));
->>>>>>> 2c112e290717192f46429debd90bb4a738969949
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -30,8 +27,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-<<<<<<< HEAD
 
 module.exports =config;
-=======
->>>>>>> 2c112e290717192f46429debd90bb4a738969949
+
